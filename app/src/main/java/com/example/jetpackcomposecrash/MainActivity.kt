652d872cccfb4bd5512411fc693e00deee89ddda
Abubakar younas <48103859+Abubakaryounas@users.noreply.github.com>
2023-05-04 13:08:22 +0500

    modified code to remember scroll state after app is minimized

--- app/src/main/java/com/example/jetpackcomposecrash/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecrash/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcomposecrash
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
@@ -19,15 +21,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposecrash.ui.theme.JetpackComposeCrashTheme
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 
 class MainActivity : ComponentActivity() {
+    private val prefs by lazy {
+        applicationContext.getSharedPreferences("myPrefs", MODE_PRIVATE)
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,7 +73,7 @@ class MainActivity : ComponentActivity() {
                             Text(text = "Add in List")
                         }
                     }
-                    DisplayNameList(namesList = namesList)
+                    DisplayNameList(namesList = namesList, prefs)
                 }
             }
         }
@@ -73,8 +83,27 @@ class MainActivity : ComponentActivity() {
 //we can also pass Modifier in function
 // if we want to change from outside
 @Composable
-fun DisplayNameList(namesList: List<String>) {
-    LazyColumn {
+fun DisplayNameList(namesList: List<String>, prefs: SharedPreferences) {
+
+    //code to remember last scrolled position of list
+    val scrollPosition = prefs.getInt("scroll_position", 0)
+    val lazyListState = rememberLazyListState(
+        initialFirstVisibleItemIndex = scrollPosition
+    )
+
+    LaunchedEffect(lazyListState) {
+        snapshotFlow {
+            lazyListState.firstVisibleItemIndex
+        }
+            //using debounce to remember state after 500 milliseconds
+            .debounce(500L)
+            .collectLatest { index ->
+                prefs.edit().putInt("scroll_position", index)
+                    .apply()
+            }
+    }
+
+    LazyColumn(state = lazyListState) {
         items(namesList) { currentName ->
             Text(
                 text = currentName,
@@ -82,6 +111,8 @@ fun DisplayNameList(namesList: List<String>) {
                     .fillMaxWidth()
                     .padding(16.dp),
             )
+            /*items(100){
+                Text(text = "hey $it")*/
             Divider()
 
         }
